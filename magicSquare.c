56d7cca2566665b4ad82fab279ece9ad446d58c8
nicoalejo <nicoalejor@gmail.com>
2023-05-31 02:08:37 -0500

    first commit

---
+++ magicSquare.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "magicSquare.h"
+
+int cuadradoMagico(int filas, int columnas, int cuadrado[filas][columnas]) {
+    
+}
+
+int calcularConstanteMagica(int filas, int columnas, int cuadrado[filas][columnas]) {
+    
+}
+
+void imprimirCuadrado(int filas, int columnas, int cuadrado[filas][columnas]) {
+    
+}
